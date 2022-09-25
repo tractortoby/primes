1f938a8d79a7d0a85c41d4f61cd49bb79252bb46
tractortoby <tobychung@duck.com>
2022-09-25 14:10:31 +0800

    useMemo usage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import format from "date-fns/format";
 
 function App() {
+  const [selectedNum, setSelectedNum] = React.useState(100);
+  const time = useTime();
+
+  const allPrimes = React.useMemo(() => {
+    const result = [];
+
+    for (let counter = 2; counter < selectedNum; counter++) {
+      if (isPrime(counter)) {
+        result.push(counter);
+      }
+    }
+
+    return result;
+  }, [selectedNum]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p className="clock">{format(time, "hh:mm:ss a")}</p>
+      <form>
+        <label htmlFor="num">Your number:</label>
+        <input
+          type="number"
+          value={selectedNum}
+          onChange={(event) => {
+            // To prevent computers from exploding,
+            // we'll max out at 100k
+            let num = Math.min(100_000, Number(event.target.value));
+
+            setSelectedNum(num);
+          }}
+        />
+      </form>
+      <p>
+        There are {allPrimes.length} prime(s) between 1 and {selectedNum}:{" "}
+        <span className="prime-list">{allPrimes.join(", ")}</span>
+      </p>
+    </>
   );
+}
+
+function useTime() {
+  const [time, setTime] = React.useState(new Date());
+
+  React.useEffect(() => {
+    const intervalId = window.setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      window.clearInterval(intervalId);
+    };
+  }, []);
+
+  return time;
+}
+
+function isPrime(n) {
+  const max = Math.ceil(Math.sqrt(n));
+
+  if (n === 2) {
+    return true;
+  }
+
+  for (let counter = 2; counter <= max; counter++) {
+    if (n % counter === 0) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 export default App;
